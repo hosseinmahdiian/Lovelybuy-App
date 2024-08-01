6abde04c1071c81e567c7e1aa97d8e7675198d7c
Hossein Mahdian.Z <hosseinmahdian81@gmail.com>
2024-08-01 09:36:09 +0330

    commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import Acconut from './Account';
 
 const Home = () => {
     return (
-      <div className="w-[400px] max-h-screen  h-[700px] mx-auto">
+      <div className="w-[400px] max-h-screen min-h-[600px] h-[900px] mx-auto">
         <Acconut />
       </div>
     );
