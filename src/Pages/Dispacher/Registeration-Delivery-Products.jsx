c4c354f0494e50bec69ba570cf87681a20199131
Hossein Mahdian.Z <hosseinmahdian81@gmail.com>
2024-08-06 13:31:03 +0330

    commit

--- src/Pages/Dispacher/Registeration-Delivery-Products.jsx
+++ src/Pages/Dispacher/Registeration-Delivery-Products.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Layout from "../../Layout/Layout";
+import { ReactSketchCanvas } from "react-sketch-canvas";
 
 const RegisterationDeliveryProducts = () => {
   return (
-    
-      <div className="mt-5 w-11/12 h-3/4 rounded-lg bg-white mx-auto">
-       
-      </div>
-   
+    <div className="mt-5 w-11/12 h-3/4 rounded-lg bg-white mx-auto">
+      <ReactSketchCanvas
+        width="100%"
+        height="100%"
+        canvasColor="transparent"
+        strokeColor="#000"
+        strokeWidth="4px"
+      />
+    </div>
   );
 };
 
